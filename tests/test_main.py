532e1e726ea515895af4ca291024af4304b9f293
Sefi Erlich <erlichsefi@gmail.com>
2022-09-09 17:28:38 +0300

    .

--- tests/test_main.py
+++ tests/test_main.py
@@ -24,10 +24,10 @@ def test_main_with_limit():
 
 
 def test_main_with_one_scarper():
-    scrapper_done = Main().start(limit=1,enabled_scrapers=["DorAlon"])
+    scrapper_done = ScarpingTask().start(limit=1,enabled_scrapers=["DorAlon"])
     assert "dor" in scrapper_done[0].lower() and "alon" in scrapper_done[0].lower()
     assert len(scrapper_done) == 1
 
 def test_main_with_size_estimation_mode():
-    scrapper_done = Main().start(limit=1,size_estimation_mode=True,enabled_scrapers=["DorAlon"])
+    scrapper_done = ScarpingTask().start(limit=1,size_estimation_mode=True,enabled_scrapers=["DorAlon"])
     assert len(scrapper_done) == 0
