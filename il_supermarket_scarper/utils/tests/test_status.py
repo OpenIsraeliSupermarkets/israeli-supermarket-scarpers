f9372a93ffb0ba767495217ddc1aa47a01b801b1
Sefi Erlich <erlichsefi@gmail.com>
2022-10-02 15:11:45 +0300

    disable tests that cant run outside israel

--- il_supermarket_scarper/utils/tests/test_status.py
+++ il_supermarket_scarper/utils/tests/test_status.py
@@ -1,12 +1,14 @@
 from il_supermarket_scarper.utils.status import get_status,get_status_date
+from il_supermarket_scarper.utils.connection import disable_when_outside_israel
 import datetime
 
 
+@disable_when_outside_israel
 def test_status():
     num_of_scarpers = get_status()
     assert type(num_of_scarpers) == int
 
-
+@disable_when_outside_israel
 def test_status_date():
     date = get_status_date()
     assert type(date) == datetime.datetime
