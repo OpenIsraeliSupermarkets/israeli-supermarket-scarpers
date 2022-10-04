f89fa1db8bbf5076e1ca465949f6d4828feb5398
Sefi Erlich <erlichsefi@gmail.com>
2022-10-04 15:10:36 +0300

    fix

--- il_supermarket_scarper/utils/status.py
+++ il_supermarket_scarper/utils/status.py
@@ -1,15 +1,17 @@
-import requests
-import lxml.html as lh
-from bs4 import BeautifulSoup
+import datetime
 import re
 import os
-import datetime 
+import enum
+import requests
 import pytz
+import lxml.html as lh
+from bs4 import BeautifulSoup
 
-    
 from .logger import Logger
 
+
 def get_status():
+    """ get the number of scarper listed on the gov.il site """
     url = "https://www.gov.il/he/departments/legalInfo/cpfta_prices_regulations"
     #Create a handle, page, to handle the contents of the website
     page = requests.get(url)
@@ -25,6 +27,7 @@ def get_status():
 
 
 def get_status_date():
+    """ get the date change listed on the gov.il site """
     url = "https://www.gov.il/he/departments/legalInfo/cpfta_prices_regulations"
     #Create a handle, page, to handle the contents of the website\
     page = requests.get(url)
@@ -32,12 +35,14 @@ def get_status_date():
     if page.status_code != 200:
         Logger.error(f"request as failed, page body is {page}.")
         raise ValueError(f"Failed reading site {url}.")
-    line_with_date = lh.fromstring(page.content).xpath('/html/body/section/div/div[3]/div/span')[0].text
-    Logger.info("line_with_date: {}".format(line_with_date))
-    
-    dates = re.findall('([1-9]|1[0-9]|2[0-9]|3[0-1]|0[0-9])(.|-|\/)([1-9]|1[0-2]|0[0-9])(.|-|\/)(20[0-9][0-9])',line_with_date)
+    line_with_date = lh.fromstring(page.content).xpath("""/html/body/section/div/
+                                                        div[3]/div/span""")[0].text
+    Logger.info(f"line_with_date: {line_with_date}")
 
-    Logger.info("Found {} dates".format(len(dates)))
+    dates = re.findall("""([1-9]|1[0-9]|2[0-9]|3[0-1]|0[0-9])(.|-|\/)
+            ([1-9]|1[0-2]|0[0-9])(.|-|\/)(20[0-9][0-9])""",line_with_date)
+
+    Logger.info(f"Found {len(dates)} dates")
     if len(dates) != 1:
         raise ValueError(f"found dates: {dates}")
 
@@ -45,8 +50,10 @@ def get_status_date():
 
 
 def get_all_listed_scarpers():
+    """ list all scrapers possible to use """
     from il_supermarket_scarper.engines.engine import Engine
     import il_supermarket_scarper.scrappers as scrappers
+
     all_scrapers = list()
     for _,value in scrappers.__dict__.items():
         if callable(value) and isinstance(value(),Engine):
@@ -54,89 +61,91 @@ def get_all_listed_scarpers():
     return all_scrapers
 
 def get_all_listed_scarpers_class_names():
+    """ get the class name of all listed scrapers """
     result = list()
     for class_instance in get_all_listed_scarpers():
         result.append( class_instance.__name__)
     return result
 
 def get_scraper_by_class(class_names):
-    for class_instance in get_all_listed_scarpers():  
+    """ get a scraper by class name """
+    for class_instance in get_all_listed_scarpers():
         if class_instance.__name__ == class_names:
             return class_instance
-    raise ValueError("class_names {} not found".format(class_names))
+    raise ValueError(f"class_names {class_names} not found")
 
 def get_output_folder(chain_name):
+    """ the the folder to write the chain fils in """
     return os.path.join(_get_dump_folder(), chain_name)
-    
+
 def _get_dump_folder():
+    """ get the dump folder to locate the chains folders in """
     return os.environ.get('XML_STORE_PATH',"dumps")
 
-import enum
-# Enum for size units
-class SIZE_UNIT(enum.Enum):
-   BYTES = "Bytes"
-   KB = "Kb"
-   MB = "Mb"
-   GB = "Gb"
-def convert_unit(size_in_bytes, unit):
-   """ Convert the size from bytes to other units like KB, MB or GB"""
-   if unit == SIZE_UNIT.KB: 
-       return size_in_bytes/1024  
-   elif unit == SIZE_UNIT.MB:
-       return size_in_bytes/(1024*1024)
-   elif unit == SIZE_UNIT.GB:
-       return size_in_bytes/(1024*1024*1024)
-   else:
-       return size_in_bytes
 
-def log_folder_details(folder,unit=SIZE_UNIT.MB):
-    from .logger import Logger
+# Enum for size units
+class UnitSize(enum.Enum):
+    """ enum represent the unit size in memory """
+    BYTES = "Bytes"
+    KB = "Kb"
+    MB = "Mb"
+    GB = "Gb"
+def convert_unit(size_in_bytes, unit):
+    """ Convert the size from bytes to other units like KB, MB or GB"""
+    if unit == UnitSize.KB:
+        return size_in_bytes/1024
+    elif unit == UnitSize.MB:
+        return size_in_bytes/(1024*1024)
+    elif unit == UnitSize.GB:
+        return size_in_bytes/(1024*1024*1024)
+    else:
+        return size_in_bytes
+
+def log_folder_details(folder,unit=UnitSize.MB):
+    """ log details about a folder """
     unit_size = 0
     for path, dirs, files in os.walk(folder):
         # summerize all files
-        Logger.info("Found the following files in {}:".format(path))
+        Logger.info(f"Found the following files in {path}:")
         size = 0
-        for f in files:
-            if "xml" in f:
-                fp = os.path.join(path, f)
+        for file in files:
+            if "xml" in file:
+                fp = os.path.join(path, file)
                 fp_size = os.path.getsize(fp)
                 size += fp_size
-                Logger.info("- file {}: size {}".format(fp, size))
-        
+                Logger.info(f"- file {fp}: size {size}")
+
         unit_size = convert_unit(size,unit)
-        Logger.info("Found the following folders in {}:".format(path))
+        Logger.info(f"Found the following folders in {path}:")
         for folder in dirs:
             unit_size += log_folder_details(os.path.join(path, folder),unit)
 
-    Logger.info("Total size of {}: {} {}".format(path, unit_size, unit.name))
-    
+    Logger.info(f"Total size of {folder}: {unit_size} {unit.name}")
+
     return {"size":unit_size,
             "unit":unit.name}
 
 def summerize_dump_folder_contant(dump_folder):
-    from .logger import Logger
-    import os
+    """ collect details about the dump folder """
 
     Logger.info(" == Starting summerize dump folder == ")
-    Logger.info("dump_folder = {}".format(dump_folder))
+    Logger.info(f"dump_folder = {dump_folder}")
     for any_file in os.listdir(dump_folder):
         current_file = os.path.join(dump_folder,any_file)
         if os.path.isdir(current_file):
             log_folder_details(current_file)
         else:
-            Logger.info("- file {}".format(current_file))
+            Logger.info(f"- file {current_file}")
 
 
 def clean_dump_folder(dump_folder):
-    from .logger import Logger
-    import os
-
+    """ clean the dump folder completly """
     for any_file in os.listdir(dump_folder):
         current_file = os.path.join(dump_folder,any_file)
         if os.path.isdir(current_file):
-            for f in os.listdir(current_file):
-                fp = os.path.join(current_file, f)
-                os.remove(fp)
+            for file in os.listdir(current_file):
+                full_file_path = os.path.join(current_file, file)
+                os.remove(full_file_path)
             os.rmdir(current_file)
         else:
             os.remove(current_file)
