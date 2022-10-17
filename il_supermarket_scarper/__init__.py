d14e03a96159cd5b32d02f8262b7931552f8bea1
Sefi Erlich <erlichsefi@gmail.com>
2022-10-17 23:48:56 +0300

    expose only main

--- il_supermarket_scarper/__init__.py
+++ il_supermarket_scarper/__init__.py
@@ -1,2 +1,1 @@
 from .main import ScarpingTask
-from .scrapper_runner import MainScrapperRunner
---
+++ main.py
@@ -0,0 +1,5 @@
+from il_supermarket_scarper import ScarpingTask
+
+
+if __name__ == "__main__":
+    ScarpingTask().start()
