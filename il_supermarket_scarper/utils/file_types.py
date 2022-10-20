2c4368648614d558f24cae918ae12e2fe5213f26
Sefi Erlich <erlichsefi@gmail.com>
2022-10-20 18:28:16 +0300

    filter by full

--- il_supermarket_scarper/utils/file_types.py
+++ il_supermarket_scarper/utils/file_types.py
@@ -4,9 +4,26 @@ from enum import Enum
 class FileTypesFilters(Enum):
     """type of files avaliable to download"""
 
-    PROMO_FILE = "promo"
-    STORE_FILE = "store"
-    PRICE_FILE = "price"
+    PROMO_FILE = {
+        "should_contain": "promo",
+        "should_not_contain": "full",
+    }
+    STORE_FILE = {
+        "should_contain": "store",
+        "should_not_contain": None,
+    }
+    PRICE_FILE = {
+        "should_contain": "price",
+        "should_not_contain": "full",
+    }
+    PROMO_FULL_FILE = {
+        "should_contain": "promofull",
+        "should_not_contain": None,
+    }
+    PRICE_FULL_FILE = {
+        "should_contain": "pricefull",
+        "should_not_contain": None,
+    }
 
     @classmethod
     def all_types(cls):
@@ -16,7 +33,7 @@ class FileTypesFilters(Enum):
     @classmethod
     def only_promo(cls):
         """only files with promotion date"""
-        return [FileTypesFilters.PROMO_FILE.name]
+        return [FileTypesFilters.PROMO_FILE.name, FileTypesFilters.PROMO_FULL_FILE.name]
 
     @classmethod
     def only_store(cls):
@@ -26,12 +43,19 @@ class FileTypesFilters(Enum):
     @classmethod
     def only_price(cls):
         """only files with prices date"""
-        return [FileTypesFilters.PRICE_FILE.name]
+        return [FileTypesFilters.PRICE_FILE.name, FileTypesFilters.PRICE_FULL_FILE.name]
 
     @staticmethod
-    def filter_file(file_name, key_name):
+    def filter_file(file_name, should_contain, should_not_contain):
         """fillter function"""
-        return key_name in file_name.lower() and "null" not in file_name.lower()
+        return (
+            should_contain in file_name.lower()
+            and "null" not in file_name.lower()
+            and (
+                should_not_contain is None
+                or should_not_contain not in file_name.lower()
+            )
+        )
 
     @classmethod
     def filter(cls, file_type, iterable, by_function=lambda x: x):
@@ -40,7 +64,7 @@ class FileTypesFilters(Enum):
         return list(
             filter(
                 lambda filename: cls.filter_file(
-                    by_function(filename), string_to_look_in
+                    by_function(filename), **string_to_look_in
                 ),
                 iterable,
             )
