11525ddde27638802aabd45504072fe3a0442de4
Sefi Erlich <erlichsefi@gmail.com>
2022-10-22 15:07:30 +0300

    fix chain ids

--- il_supermarket_scarper/scrappers/yohananof.py
+++ il_supermarket_scarper/scrappers/yohananof.py
@@ -7,7 +7,7 @@ class Yohananof(Cerberus):
     def __init__(self, folder_name=None):
         super().__init__(
             "Yohananof",
-            chain_id="290803800003",
+            chain_id="7290803800003",
             folder_name=folder_name,
             ftp_username="yohananof",
         )
