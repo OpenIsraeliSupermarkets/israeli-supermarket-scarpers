b14acb39cbccd50d110986b30446a6f17fb8907e
Sefi Erlich <erlichsefi@gmail.com>
2022-10-22 15:27:14 +0300

    more ids

--- il_supermarket_scarper/scrappers/stop_market.py
+++ il_supermarket_scarper/scrappers/stop_market.py
@@ -7,7 +7,10 @@ class StopMarket(Cerberus):
     def __init__(self, folder_name=None):
         super().__init__(
             "Stop Market",
-            chain_id="72906390",
+            chain_id=[
+                "72906390",
+                "7290639000004",
+            ],  # in store files for some reason the store id is only 72906390
             folder_name=folder_name,
             ftp_username="Stop_Market",
         )
