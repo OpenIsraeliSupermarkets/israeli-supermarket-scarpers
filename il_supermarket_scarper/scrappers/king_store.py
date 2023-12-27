987b518b4082b01e85fdedb3d134ee819642b2c7
Erlich Sefi <erlichsefi@gmail.com>
2023-12-27 16:18:34 +0000

    .

--- il_supermarket_scarper/scrappers/king_store.py
+++ il_supermarket_scarper/scrappers/king_store.py
@@ -8,7 +8,6 @@ class KingStore(Bina):
         super().__init__(
             chain="King Store",
             chain_id="7290058108879",
-            url_perfix="www",
-            domain="kingstore.co.il/Food_Law/",
-            folder_name=folder_name,
+            url_perfix="kingstore",
+            folder_name=folder_name
         )
