f9ff203eff6bd5c4f8d5b0f6cc0b55f8e72f86a1
Sefi Erlich <erlichsefi@gmail.com>
2024-01-13 22:09:18 +0200

    increase version

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
     tests_require=dev_required,
     extras_require={"test": ["pytest"]},
     # *strongly* suggested for sharing
-    version="0.3.6",
+    version="0.3.7",
     # The license can be anything you like
     license="MIT",
     description="python package that implement a scraping for israeli supermarket data",
