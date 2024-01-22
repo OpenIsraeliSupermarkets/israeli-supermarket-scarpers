7b38bf42ac2cfadc560dd719ed46b91fb45acca6
Sefi Erlich <erlichsefi@gmail.com>
2024-01-22 21:29:00 +0200

    flaky

--- il_supermarket_scarper/scrappers/quik.py
+++ il_supermarket_scarper/scrappers/quik.py
@@ -1,6 +1,7 @@
 from il_supermarket_scarper.engines.publishprice import PublishPrice
+from il_supermarket_scarper.utils import FlakyScraper
 
-
+@FlakyScraper
 class Quik(PublishPrice):
     """scaper for quik"""
 
