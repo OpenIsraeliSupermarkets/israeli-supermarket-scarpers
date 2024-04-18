ba6cbe6bdb1a772787acd05f33476bb45fbfdb98
Sefi Erlich <erlichsefi@gmail.com>
2024-04-18 06:20:16 +0000

    .

--- test.py
+++
@@ -1,9 +0,0 @@
-from playwright.sync_api import sync_playwright
-
-with sync_playwright() as p:
-    for browser_type in [p.chromium, p.firefox, p.webkit]:
-        browser = browser_type.launch()
-        page = browser.new_page()
-        page.goto('https://www.gov.il/he/pages/cpfta_prices_regulations')
-        print(page.content())
-        browser.close()
