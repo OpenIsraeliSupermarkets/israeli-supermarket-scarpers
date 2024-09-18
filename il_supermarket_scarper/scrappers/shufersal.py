645b63f34c8eb2b460c83d646dfc56666c1abe5f
Sefi Erlich <erlichsefi@gmail.com>
2024-09-18 17:20:29 +0000

    testing

--- il_supermarket_scarper/scrappers/shufersal.py
+++ il_supermarket_scarper/scrappers/shufersal.py
@@ -1,4 +1,5 @@
 from il_supermarket_scarper.engines import MultiPageWeb
+from il_supermarket_scarper.utils import DumpFolderNames
 
 
 class Shufersal(MultiPageWeb):
@@ -7,7 +8,7 @@ class Shufersal(MultiPageWeb):
     def __init__(self, folder_name=None):
         super().__init__(
             url="https://prices.shufersal.co.il/",
-            chain="Shufersal",
+            chain=DumpFolderNames.SHUFERSAL,
             chain_id="7290027600007",
             folder_name=folder_name,
         )
