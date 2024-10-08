8fcd82f4447897c0272e9e2d0b6186a01b1375ac
Sefi Erlich <erlichsefi@gmail.com>
2024-10-08 19:46:31 +0000

    .

--- il_supermarket_scarper/utils/gzip_utils.py
+++ il_supermarket_scarper/utils/gzip_utils.py
@@ -13,18 +13,18 @@ def extract_xml_file_from_gz_file(file_save_path):
         with gzip.open(file_save_path, "rb") as infile:
             with open(target_file_name, "wb") as outfile:
                 shutil.copyfileobj(infile, outfile)
-    except (gzip.BadGzipFile,EOFError) as exception:
+    except (gzip.BadGzipFile, EOFError) as exception:
         try:
             with open(file_save_path, "rb") as response_content:
                 with zipfile.ZipFile(io.BytesIO(response_content.read())) as the_zip:
                     zip_info = the_zip.infolist()[0]
                     with the_zip.open(zip_info) as the_file:
-                        with open(
-                            target_file_name, "wb"
-                        ) as f_out:
+                        with open(target_file_name, "wb") as f_out:
                             f_out.write(the_file.read())
 
-        except Exception as exception:  # pylint: disable=broad-except
+        except (
+            Exception
+        ) as exception:  # pylint: disable=broad-except,redefined-outer-name
             report_failed_zip(exception, file_save_path, target_file_name)
 
     except Exception as exception:  # pylint: disable=broad-except
@@ -33,15 +33,13 @@ def extract_xml_file_from_gz_file(file_save_path):
 
 def report_failed_zip(exception, file_save_path, target_file_name):
     """report a file wasn't able to extracted"""
-    
+
     try:
         file_size = os.path.getsize(file_save_path)
-        
+
         file_contant = ""
         with open(file_save_path, "r", encoding="utf-8") as file:
             file_contant = file.readlines()
-        
-        
 
         if "link expired" in str(file_contant):
             raise RestartSessionError()
@@ -56,7 +54,7 @@ def report_failed_zip(exception, file_save_path, target_file_name):
             f"Error decoding file:{file_save_path} with "
             f"error: {str(exception)} file size {str(file_size)} ,"
             f"can't decode file"
-            )
+        )
     finally:
         os.remove(file_save_path)
         # remove the corrupted file
--- il_supermarket_scarper/utils/tests/test_connections.py
+++
@@ -1,5 +0,0 @@
-from il_supermarket_scarper.utils.connection import session_and_check_status
-
-
-def test_session_and_check_status():
-    session_and_check_status("https://www.google.com")
