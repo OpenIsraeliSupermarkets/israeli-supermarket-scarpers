f471f68168597f552db4536edf144080924dfadc
Sefi Erlich <erlichsefi@gmail.com>
2024-12-12 18:41:05 +0000

    .

--- il_supermarket_scarper/tests/test_scrappers_factory.py
+++ il_supermarket_scarper/tests/test_scrappers_factory.py
@@ -3,12 +3,14 @@ from il_supermarket_scarper.utils import _is_saturday_in_israel
 
 
 def test_stable_scraper():
+    """test sample stable scarper"""
     assert not ScraperStability.is_validate_scraper_found_no_files(
         ScraperFactory.VICTORY.name
     )
 
 
 def test_after_date():
+    """test scrapers that failed after date"""
     assert ScraperStability.is_validate_scraper_found_no_files(
         ScraperFactory.CITY_MARKET_GIVATAYIM.name,
         when_date=datetime_in_tlv(2024, 12, 12, 0, 0, 0),
@@ -16,6 +18,7 @@ def test_after_date():
 
 
 def test_not_active():
+    """test grap between active and not"""
     all_listed = ScraperFactory.all_listed_scrappers()
     all_active = ScraperFactory.all_scrapers_name(
         when_date=datetime_in_tlv(2024, 12, 12, 0, 0, 0)
