6ac91987ea38a6ea60c50ea342000b83a78292ef
Sefi Erlich <erlichsefi@gmail.com>
2026-01-17 19:19:49 +0000

    setup

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(
     tests_require=dev_required,
     extras_require={"test": ["pytest", "pytest-xdist"]},
     # *strongly* suggested for sharing
-    version="0.6.5",
+    version="1.0.0",
     # The license can be anything you like
     license="MIT",
     description="python package that implement a scraping for israeli supermarket data",
