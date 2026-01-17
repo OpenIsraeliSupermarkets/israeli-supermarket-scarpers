7ee082ef228bdc31060b863305f1ebe97cb9d37e
Sefi Erlich <erlichsefi@gmail.com>
2026-01-17 19:11:54 +0000

    .

--- il_supermarket_scarper/utils/tests/test_status.py
+++ il_supermarket_scarper/utils/tests/test_status.py
@@ -1,5 +1,5 @@
 import datetime
-
+from html_diff import diff
 from il_supermarket_scarper.utils.status import (
     get_status,
     get_status_date,
@@ -21,11 +21,3 @@ def test_status_date():
     """check able the get the date the gov.il site was updated"""
     date = get_status_date()
     assert isinstance(date, datetime.datetime)
-
-
-@disable_when_outside_israel
-def test_page_complete_diff():
-    """make sure the page content is the same as the cached page"""
-    cached = get_statue_page(extraction_type="all_text", source="cache")
-    current = get_statue_page(extraction_type="all_text", source="gov.il")
-    assert current == cached, show_text_diff(cached, current)
