c40744eb34414ad1e91c79b0756168912b30c571
Erlich Sefi <erlichsefi@gmail.com>
2026-08-29 21:52:14 +0300

    Bump version from 1.0.8 to 1.0.9

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setup(
     tests_require=dev_required,
     extras_require={"test": ["pytest", "pytest-xdist"]},
     # *strongly* suggested for sharing
-    version="1.0.8",
+    version="1.0.9",
     # The license can be anything you like
     license="CUSTOM",
     description="python package that implement a scraping for israeli supermarket data",
